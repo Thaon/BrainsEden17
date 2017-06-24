e926877c46d4d1819d61ec506be664c70c3a88d8
YAP! <gabriele.maddaloni@gmail.com>
2017-06-24 13:11:03 +0100

    made petrol manager and petrol drop classes

---
+++ Assets/Code/PetrolDrop.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(LineRenderer))]
+public class PetrolDrop : MonoBehaviour {
+
+    #region member variables
+
+    public GameObject m_lastDroppedPetrol;
+
+    private LineRenderer m_line;
+
+    #endregion
+
+    void Start ()
+    {
+        
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void SetupTrail(GameObject lastDrop)
+    {
+        if (m_line == null)
+            m_line = GetComponent<LineRenderer>();
+
+        m_lastDroppedPetrol = lastDrop;
+        m_line.SetPosition(0, transform.position);
+        m_line.SetPosition(1, m_lastDroppedPetrol.transform.position);
+    }
+}
---
+++ Assets/Code/PetrolManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PetrolManager : MonoBehaviour
+{
+    #region member variables
+
+    public GameObject m_petrolPrefab;
+    public float m_petrolDropRadius;
+
+    private GameObject m_lastDroppedPetrol = null;
+
+    #endregion
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		if (m_lastDroppedPetrol != null)
+        {
+            //we are always the end of the last dropped petrol trail
+            m_lastDroppedPetrol.GetComponent<PetrolDrop>().SetupTrail(this.gameObject);
+
+            //only need 2D distance
+            Vector3 pos = transform.position;
+            Vector3 petrolPos = m_lastDroppedPetrol.transform.position;
+
+            pos.y = 0;
+            petrolPos.y = 0;
+
+            if (Vector3.Distance(pos, petrolPos) > m_petrolDropRadius)
+            {
+                GameObject drop = Instantiate(m_petrolPrefab, transform.position, Quaternion.identity);
+                drop.GetComponent<PetrolDrop>().SetupTrail(m_lastDroppedPetrol);
+                m_lastDroppedPetrol = drop;
+            }
+        }
+        else
+        {
+            GameObject drop = Instantiate(m_petrolPrefab, transform.position, Quaternion.identity);
+            m_lastDroppedPetrol = drop;
+        }
+	}
+}
